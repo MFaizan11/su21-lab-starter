2136b5fc10c32defcbab574919ac2330286e688e
MFaizan11 <muhammadfaizanm00@gmail.com>
2023-07-24 17:41:37 +0500

    Tested on cpplint

--- lab01/exercise1/ex1.c
+++ lab01/exercise1/ex1.c
@@ -1,3 +1,4 @@
+// "Copyright [2023] Muhammad Faizan"
 #include <string.h>
 #include "ex1.h"
 
@@ -7,15 +8,14 @@ There are two different ways to iterate through a string.
 2nd way hint: all strings end in a null terminator */
 int num_occurrences(char *str, char letter) {
     /* TODO: implement num_occurances */
-    int n = 0;
-    int length = strlen(str);
-    for (int i=0;i<length;i++)
-    {
-        if (letter == str[i])
-        {
-            n += 1;   
-        }
+  int n = 0;
+  int length = strlen(str);
+
+  for (int i = 0; i < length; i++) {
+    if (letter == str[i]) {
+      n += 1;
     }
+  }
     return n;
 }
 
@@ -25,11 +25,9 @@ All letters will be upper case. */
 void compute_nucleotide_occurrences(DNA_sequence *dna_seq) {
     /* TODO: implement compute_nucleotide_occurances */
     int length = strlen(dna_seq->sequence);
-    
-    for(int i=0;i<length;i++)
-    {
-        switch (dna_seq->sequence[i])
-        {
+
+    for (int i = 0; i < length; i++) {
+        switch (dna_seq->sequence[i]) {
         case 'A':
             dna_seq->A_count++;
             break;
