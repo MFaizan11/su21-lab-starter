f906807fbaef21b4a3b4c825a12ede6712f0d809
MFaizan11 <muhammadfaizanm00@gmail.com>
2023-08-07 01:22:24 +0500

    Programming Task

--- riscv/task2.s
+++ riscv/task2.s
@@ -1,51 +1,52 @@
+.data
+A: .space 40          # allocate an array with space for 10 integers (40 bytes)
+
 .text
-.globl main
-
 main:
-    addi a0, x0, 2    # set num to 5
-    jal ra, dataArray # call dataArray function
-
-dataArray:
-    addi sp, sp, -44  # allocate stack space for the array and saved registers
-    sw ra, 40(sp)     # save the return address
-    sw s0, 36(sp)     # save s0
-    addi s0, x0, 0    # i = 0
-    addi s1, sp, 0    # base of the array
-loop:
-    addi s4, x0, 10
-    bge s0, s4, exit  # if i >= 10, then exit loop
-    add a2, s0, x0    # set second argument of compare to i
-    add a1, a0, x0    # set first argument of compare to num
-    jal ra, compare   # call compare
-
-    sw a0, 0(s1)      # store the result of compare in array[i]
-
-    # Print the value
-    mv a1, a0         # move the value to a1
-    addi a0, x0, 1    # set a7 to 1 (syscall for print integer)
-    ecall             # print integer syscall
-
-    addi s0, s0, 1    # increment i
-    addi s1, s1, 4    # move to the next position in the array
-    j loop            # jump back to the start of loop
-
+    addi s0, x0, 0        # initialize 0 to i
+    addi a0, x0, 9        # set num to 9
+    la s1, A              # pointer to the start of array A
+    addi t1, s1, 40       # pointer to the end of array A
+    jal ra, data_array    # data_array function calling
 exit:
-    lw ra, 40(sp)     # restore the return address
-    lw s0, 36(sp)     # restore s0
-    addi sp, sp, 44   # deallocate stack space
-    jr ra             # return from dataArray
-
+    addi a0, x0, 10       # exit
+    ecall
 compare:
-    add a2, a1, x0    # copy the first argument to a2
-    add a1, a0, x0    # copy the second argument to a1
-    jal ra, sub       # call sub
-    blt a0, x0, else  # if result of sub < 0, jump to else
-    addi a0, x0, 1    # return 1
-    jr ra             # return from compare
-else:
-    addi a0, x0, 0    # return 0
-    jr ra             # return from compare
-
-sub:
-    sub a0, a0, a1    # return a - b
-    jr ra             # return from sub
+    addi sp, sp, -8       # allocate stack space for saving registers
+    sw ra, 0(sp)          # save the return address
+    jal ra, subtract      # subtract function calling
+    lw t0, 0(sp)          # load the result of the subtraction
+    bge t0, x0, exit1     # if the result is >= 0, go to exit1
+    sw x0, 8(sp)          # else, return 0
+    j return
+subtract:
+    lw t2, 8(sp)          # load the value of i
+    lw t3, 12(sp)         # load the value of num
+    sub t3, t3, t2        # subtract i from num
+    addi sp, sp, -4       # make space for the result on the stack
+    sw t3, 0(sp)          # store the result on the stack
+    jr ra                 # return to caller
+return:
+    lw ra, 4(sp)          # restore the return address
+    addi sp, sp, 8        # deallocate the stack space
+    jr ra                 # return to caller
+exit1:
+    addi t2, x0, 1
+    sw t2, 8(sp)          # return 1
+    j return
+data_array:
+    sw ra, 0(sp)          # save the return address
+    addi sp, sp, -8       # allocate stack space
+    sw a0, 4(sp)          # store num on the stack
+for_loop:
+    sw s0, 0(sp)          # store i on the stack
+    jal ra, compare       # call compare function
+    lw t3, 0(sp)          # load the result of compare function
+    addi sp, sp, 4        # deallocate stack space for i
+    sw t3, 0(s1)          # store the result in the array
+    addi s0,s0, 1         # increment i
+    addi s1, s1, 4        # move the array pointer to next integer
+    blt s1, t1, for_loop  # if end not reached continue loop
+    addi sp, sp, 8        # deallocate stack space for num and return address
+    lw ra, 0(sp)          # restore the return address
+    jr ra                 # return to caller
